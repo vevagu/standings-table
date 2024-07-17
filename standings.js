c3e05743a6b4c327ad8606cb34d5fd9e160f722f
vevagu <34694339+vevagu@users.noreply.github.com>
2024-07-16 17:49:38 -0700

    Update standings.js

--- standings.js
+++ standings.js
@@ -1,8 +1,25 @@
 // Data for Tournament would be updated here
 const standingsData = [
-    { team: "Leopards FC", played: 5, won: 3, drawn: 1, lost: 1, points: 10 },
-    { team: "Phantom FC", played: 5, won: 2, drawn: 2, lost: 1, points: 8 },
-    { team: "Total Futbol", played: 5, won: 1, drawn: 1, lost: 3, points: 4 }
+    { team: "Leopards FC", played: 0, won: 0, drawn: 0, lost: 0, points: 0 },
+    { team: "Phantom FC", played: 0, won: 0, drawn: 0, lost: 0, points: 0 },
+    { team: "Total Futbol", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Unity FC", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "DM United ", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Tsa98red ", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Blue Angels - Women C", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Indomitable Lions", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Infinity", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Amigos", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "BULLDOGS", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "DGO", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Fire Ferrets ", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Not Like Us", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Phoenix FC", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "MeliMo", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "PHX MLS", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Team Name", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    { team: "Team Name", played: 0, won: 0, drawn: 0, lost: 0, points: 0 }
+    
 ];
 
 function updateStandingsTable() {
